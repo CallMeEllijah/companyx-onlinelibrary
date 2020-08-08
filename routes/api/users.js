b5e1923025ac6749eafa0ad13fa459c0da703a0a
CallMeEllijah <CallMeEllijah@users.noreply.github.com>
2020-08-09 03:27:46 +0800

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -171,7 +171,9 @@ router.post("/login", (req, res) => {
 });
 
 router.post("/deleteUser", (req, res) => {
-  User.deleteOne({ email: req.body.email }).then(res => {return res.status(200)}).catch(err => {return res.status(400)});
+  User.deleteOne({ email: req.body.email })
+    .then(res => {return res.status(200).json({success: "worked"})})
+    .catch(err => {return res.status(400).json({error: err})});
 });
 
 module.exports = router;
