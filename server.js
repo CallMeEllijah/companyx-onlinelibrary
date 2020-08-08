3a42d4e070f2605988806604b35f90dfdbbe5ea1
CallMeEllijah <CallMeEllijah@users.noreply.github.com>
2020-08-08 23:34:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,11 @@ app.use("/api/users", users);
 app.use("/api/books", books);
 app.use("/api/logs", logs);
 
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
