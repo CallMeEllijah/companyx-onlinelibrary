66bcd5f9ee39360d1217803d62569186b890f991
CallMeEllijah <CallMeEllijah@users.noreply.github.com>
2020-08-09 01:27:31 +0800

    Revert "Revert "Update package.json""
    This reverts commit 99e12959c90fec95a0ecb82e0b8a73d70d0ecffb.

---
+++ client/build/precache-manifest.f6a402cc0dd5bf98b66a7e62a493932d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfaf72781a0daba24ad0",
+    "url": "/static/css/main.d5d17a68.chunk.css"
+  },
+  {
+    "revision": "cfaf72781a0daba24ad0",
+    "url": "/static/js/main.cfaf7278.chunk.js"
+  },
+  {
+    "revision": "4269dac26396e6a18395",
+    "url": "/static/js/1.4269dac2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9facf3cb8bf34370bdf7e4476b8f12a9",
+    "url": "/index.html"
+  }
+];
